c2bfc353eb680327fe3889c18e9f1d5bd90377fe
Aixufey <jack_11728@msn.com>
2024-07-16 19:38:24 +0200

    task10

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,25 +5,46 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  const { username, password } = req.body;
-  if (!username || !password) return res.status(400).json({ message: "Username and password are required" });
+    //Write your code here
+    const { username, password } = req.body;
+    if (!username || !password)
+        return res
+            .status(400)
+            .json({ message: "Username and password are required" });
 
-  if (isValid(username)) {
-    return res.status(400).json({ message: "Username already exists" });
-  }
-  
-  users.push({
-      username,
-      password,
-  });
-  return res.status(201).json({ message: `User ${username} registered successfully`})
+    if (isValid(username)) {
+        return res.status(400).json({ message: "Username already exists" });
+    }
+
+    users.push({
+        username,
+        password,
+    });
+    return res
+        .status(201)
+        .json({ message: `User ${username} registered successfully` });
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+public_users.get("/", async function (req, res) {
     //Write your code here
-    return res.status(200).json(books);
+    new Promise((res, rej) => {
+        setTimeout(() => {
+            if (books) {
+                res(books);
+            } else {
+                rej("No books found");
+            }
+        }, 2500);
+    }).then((books) => {
+        return res
+            .status(200)
+            .json(books)
+    }).catch((err) => {
+        return res
+            .status(404)
+            .json({ message: err });
+    });
 });
 
 // Get book details based on ISBN
@@ -41,38 +62,42 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  const { author } = req.params;
-  const foundBook = Object.values(books).find(book => book.author === author);
-  if (foundBook) {
-      return res.status(200).json(foundBook);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
-  }
+    //Write your code here
+    const { author } = req.params;
+    const foundBook = Object.values(books).find(
+        (book) => book.author === author
+    );
+    if (foundBook) {
+        return res.status(200).json(foundBook);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  const { title } = req.params;
-  const foundBook = Object.values(books).find((book) => book.title === title);
-  if (foundBook) {
-      return res.status(200).json(foundBook);
-  } else {
-      return res.status(404).json({ message: "Book not found" });
-  }
+    //Write your code here
+    const { title } = req.params;
+    const foundBook = Object.values(books).find((book) => book.title === title);
+    if (foundBook) {
+        return res.status(200).json(foundBook);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  const { isbn } = req.params;
-  const book = books[isbn];
-  if (book && book.reviews) {
-    return res.status(200).json(book.reviews);
-  } else {
-    return res.status(404).json({ message: "Book not found or no reviews" });
-  }
+    //Write your code here
+    const { isbn } = req.params;
+    const book = books[isbn];
+    if (book && book.reviews) {
+        return res.status(200).json(book.reviews);
+    } else {
+        return res
+            .status(404)
+            .json({ message: "Book not found or no reviews" });
+    }
 });
 
 module.exports.general = public_users;
