0fc6e66e8f02607dcd4dda4cb14e7be3804206e1
Aixufey <jack_11728@msn.com>
2024-07-16 11:17:13 +0200

    get book by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
     //Write your code here
-    return res.status(300).json(books);
+    return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
@@ -22,7 +22,7 @@ public_users.get("/isbn/:isbn", function (req, res) {
     //ID will be used as ISBN
     const book = books[isbn];
     if (book) {
-        return res.status(300).json(book);
+        return res.status(200).json(book);
     } else {
         return res.status(404).json({ message: "Book not found" });
     }
@@ -30,8 +30,14 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+  //Write your code here
+  const { author } = req.params;
+  const foundBook = Object.values(books).find(book => book.author === author);
+  if (foundBook) {
+      return res.status(200).json(foundBook);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Get all books based on title
